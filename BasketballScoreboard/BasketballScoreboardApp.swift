86c7968027d98aa69f9038569e50845d42d956f0
fizzw <qwe999333@gmail.com>
2025-02-23 16:26:04 +0900

    Initial Commit

---
+++ BasketballScoreboard/BasketballScoreboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasketballScoreboardApp.swift
+//  BasketballScoreboard
+//
+//  Created by 박정우 on 2/23/25.
+//
+
+import SwiftUI
+
+@main
+struct BasketballScoreboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BasketballScoreboardTests/BasketballScoreboardTests.swift
@@ -0,0 +1,17 @@
+//
+//  BasketballScoreboardTests.swift
+//  BasketballScoreboardTests
+//
+//  Created by 박정우 on 2/23/25.
+//
+
+import Testing
+@testable import BasketballScoreboard
+
+struct BasketballScoreboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
